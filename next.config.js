57e8f0842f8279062af28cae698bfb847f0c177b
Matheus <matheusalmeida016@gmail.com>
2021-12-28 13:24:39 -0400

    Update PWA

--- next.config.js
+++ next.config.js
@@ -1,10 +1,25 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const withPWA = require('next-pwa')
-const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = withPWA({
+  webpack5: true,
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback.fs = false
+    }
+
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack']
+    })
+
+    return config
+  },
+  reactStrictMode: true,
   pwa: {
     dest: 'public',
-    disable: !isProd
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === 'development'
   }
 })
